6b876609210cfe0a1d00d27b483fbad56d92b814
Akilex123 <aleksajeretin1@gmail.com>
2025-08-14 13:25:46 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,52 +1,77 @@
 package org.example;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.HashMap;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static ArrayList<Zadatak> zadaci = new ArrayList<>();
+    // Treba da napravim Hashmap<String, Double> inventar
+    public static HashMap<String, Double> inventar = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner sken = new Scanner(System.in);
         int izbor;
+        //Napravi tako da se ispise ceo meni
         do {
-            System.out.println("Izaberite opciju koju zelite");
-            System.out.println("1 - dodaj zadatak");
-            System.out.println("2 - prikazi sve zadatake");
-            System.out.println("3 - zasvrsi program");
+            System.out.println("Meni");
+            System.out.println("***********");
+            System.out.println("1 - Dodaj proizvod");
+            System.out.println("2 - Prikazi sve proizvode");
+            System.out.println("3 - Pronadji proizvod po nazivu");
+            System.out.println("4 - Izlaz");
+            System.out.print("Izaberite vas broj: ");
+
             izbor = sken.nextInt();
             sken.nextLine();
+
+            //Napravi switch tako da za svaki broj bude odradjena jedna funkcija
             switch (izbor){
-                case 1 -> dodajZadatak(sken);
-                case 2 -> prikazZadataka(sken);
-
+                case 1 -> dodajProizvod(sken);
+                case 2 -> prikazProizvoda();
+                default -> System.out.println("Niste uneli dobar broj");
             }
+        } while (izbor != 4);
 
 
-        } while (izbor != 3);
 
-    }
 
-    public static void dodajZadatak(Scanner sken){
-        System.out.println("Unesite tekst zadatka: ");
-        String tekst = sken.nextLine();
-        System.out.println("Da li je zadatak zavrsen: ");
-        boolean zavrsen = sken.nextBoolean();
-        sken.nextLine();
 
-        Zadatak noviZadatak = new Zadatak(tekst, zavrsen);
-        zadaci.add(noviZadatak);
-        System.out.println("Zadatak dodat!");
-    }
 
-    public static void prikazZadataka(Scanner sken){
-        if (zadaci.isEmpty()){
-            System.out.println("Lista je prazna!");
+
+
+
+
+
+
+
+
+
+        // Izmena cene opciona
+
+
+
         }
-        else {
-            for (Zadatak k: zadaci){
-                k.zadatakInfo();
-            }
+    // Metoda za dodavanje proizvoda
+    public static void dodajProizvod(Scanner sken){
+        System.out.print("Koji je naziv vaseg proizvoda? ");
+        String naziv = sken.nextLine();
+        System.out.print("Koja je cena vaseg proizvoda? ");
+        Double cena = sken.nextDouble();
+        inventar.put(naziv,cena);
+        System.out.println("Proizvod dodat!");
+    }
+    // Metoda za prikaz svih proizvoda sa for
+    public static void prikazProizvoda(){
+        for (String i : inventar.keySet()){
+            System.out.println("naziv: " + i + ", cena: " + inventar.get(i));
         }
     }
-}
+    // Pronalazenje proizvoda po nazivu
+    public static void pronalazenjeProizvoda(Scanner sken){
+        
+    }
+
+    }
+
+
