28c39184f14f63e67fab84264eab938314ff5630
Akilex123 <aleksajeretin1@gmail.com>
2025-09-10 15:50:46 +0200

    DSA fibonacci practice

--- src/main/java/org/example/BinaryTree.java
+++ src/main/java/org/example/BinaryTree.java
@@ -17,7 +17,22 @@ class TreeNode {
          this.right = right;
      }
  }
-
+class Solution {
+    public int tribonacci(int n) {
+        if (n<3) return n==0 ? 0:1;
+        int a = 0;
+        int b = 1;
+        int c  = 1;
+        int sum;
+        for (int i = 3; i<=n; i++){
+            sum = a + b + c;
+            a = b;
+            b = c;
+            c = sum;
+        }
+        return c;
+    }
+}
 
 
 
