08eb0cdbfa563d05d3c6f9285ddf61d0a0697a44
Akilex123 <aleksajeretin1@gmail.com>
2025-09-12 18:29:54 +0200

    Leetcode Array algos practice

--- src/main/java/org/example/BinaryTree.java
+++ src/main/java/org/example/BinaryTree.java
@@ -18,14 +18,23 @@ class TreeNode {
      }
  }
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] minCost = new int[n+1];
-        for (int i = 2; i<=n; i++){
-            minCost[i] = Math.min((cost[i-1] + minCost[i-1]),
-                    (cost[i-2] + minCost[i-2]));
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int [nums.length];
+        int[] right = new int[nums.length];
+
+        left[0] = 1;
+        for (int i=1; i<nums.length; i++){
+            left[i] = left[i-1] * nums[i-1];
         }
-        return minCost[n];
+        right[nums.length - 1] = 1;
+        for (int i=nums.length-2; i>-1; i--){
+            right[i]=right[i+1]*nums[i+1];
+        }
+        int[] ans = new int[nums.length];
+        for (int i=0; i<nums.length; i++){
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
     }
 }
 
